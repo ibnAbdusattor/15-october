b0a3af952659ef6439a9879343be3ccd5ada2166
ibnAbdusattor <omonboyevshohjahon7@gmail.com>
2025-10-15 14:22:01 +0500

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Developer: Shohjahon Omonboyev
+ * User : User
+ * Date: 15-Oct-25
+ * Time: 14:20
+ * To change this use File | Settings | Editor | File and Code Templates | Includes | File Header
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
